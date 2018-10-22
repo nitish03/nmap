80eebde6f3f9b8b3f0a85d5e3f40c0598d1cfba1
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-22 14:34:31 -0400

    add Map.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from './components/Map';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
-        <p>check git update</p>
-        <p>check git update</p>
-        <h1>Hello World</h1>
-        <p>check git update</p>
-        <p>check git update</p>
-        <h1>Hello World</h1>
-        <p>check git update</p>
+        <Map />
       </div>
     );
   }
