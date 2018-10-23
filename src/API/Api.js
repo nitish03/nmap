5a1e5a2ec31316dcaf84edf59ffbda7891d87302
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-23 16:15:31 -0400

    add foursquare keys

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,4 +2,12 @@ class Square  {
 	static rootURL() {
 		return "https://api.foursquare.com/v2";
 	}
+	static auth() {
+		const keys = {
+			client_id: "1MOFPSZVAUKPPKPUEQUNZJVBZRVKY4DO020P0LRVO2PBRVQ3",
+			client_secret: "VQVZJ1JXPSAUUDKPBVDCZ2JYPRMNHWX2GFADXTZGIMDXRCE1",
+			v: "20181022"
+		}
+		return Object.keys(keys).map(key => `${key}=${keys[key]}`).join("&");
+	}
 }
