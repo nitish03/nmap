eed34ee9f774060398a784c69e46a38f7f28ac06
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-23 17:09:30 -0400

    add componentDidMount to get API venues details in console

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Map from './components/Map';
 import SquareAPI from './API/Api';
 
 class App extends Component {
+  componentDidMount() {
+    SquareAPI.search({
+      near: "Jacksonville,FL",
+      query: "pizza",
+      limit: 15
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
