000caf2adacbf49a7c2388f6e0d68db61e4ccf6d
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-23 17:47:15 -0400

    add constructor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Map from './components/Map';
 import SquareAPI from './API/Api';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props) {
+      this.state = {
+        venues: [],
+        markers: [],
+        center: [],
+        zoom: 11
+      }
+    }
+  }
+
   componentDidMount() {
     SquareAPI.search({
       near: "Jacksonville,FL",
@@ -11,6 +23,7 @@ class App extends Component {
       limit: 15
     }).then(results => console.log(results));
   }
+
   render() {
     return (
       <div className="App">
