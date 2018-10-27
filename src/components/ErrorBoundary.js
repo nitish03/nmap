7491b5818e17fc7cc63043302459b7a1543bc8ab
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-27 16:18:38 -0400

    add componentDidCatch for ErrorBoundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class ErrorBoundary extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {hasError: false}
+		this.state = {hasError: false, errorInfo: null}
+	}
+
+	componentDidCatch(error, info) {
+		this.setState({hasError: true, errorInfo: info})
 	}
 	render() {
 		return(
