6c3399b41beac36e677e54726ecb23444050c212
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-10-27 21:33:12 -0400

    add comments for ErrorBoundary.js

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -6,9 +6,12 @@ class ErrorBoundary extends Component {
 		this.state = {hasError: false, errorInfo: null}
 	}
 
+/*show fallback UI*/
 	componentDidCatch(error, info) {
 		this.setState({hasError: true, errorInfo: info})
 	}
+
+	/*render fallback UI*/
 	render() {
 		if(this.state.hasError) {
 			return <h1>Something went wrong. Please try again.</h1>;
